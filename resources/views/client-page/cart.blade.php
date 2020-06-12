d551757e68ea29047728b49001153d74d8a1ebdc
NamanV19 <namanvohra19@gmail.com>
2020-06-12 14:36:26 +0700

    Cart insert fixed, update added, and checkout bug fixed

--- resources/views/client-page/cart.blade.php
+++ resources/views/client-page/cart.blade.php
@@ -25,7 +25,6 @@
                             </ul>
                         </div>
                     @endif
-                    <a class="btn amado-btn mb-15" href="/cart/update">Update Cart</a>
 
                     <div class="cart-table clearfix">
                         <table class="table table-responsive">
@@ -38,34 +37,39 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @forelse ($cartDetail as $cart)
-                                    <tr>
-                                        <td class="cart_product_img">
-                                            <a href="{{ url('/product-details', $cart->product_id) }}"><img src="{{ asset('images/'.$cart->product->photos->first()->image_name) }}" alt="Product"></a>
-                                        </td>
-                                        <td class="cart_product_desc">
-                                            <h5>{{ $cart->product->product_name }}</h5>
-                                        </td>
-                                        <td class="price">
-                                            <span>IDR {{ $cart->product->price }}</span>
-                                        </td>
-                                        <td class="qty">
-                                            <div class="qty-btn d-flex">
-                                                <p>Qty</p>
-                                                <div class="quantity">
-                                                    <span class="qty-minus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty ) &amp;&amp; qty &gt; 1 ) effect.value--;return false;"><i class="fa fa-minus" aria-hidden="true"></i></span>
-                                                    <input type="number" class="qty-text" id="qty" step="1" min="1" max="300" name="quantity" value="{{ $cart->qty }}">
-                                                    <span class="qty-plus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty )) effect.value++;return false;"><i class="fa fa-plus" aria-hidden="true"></i></span>
+                                <form action="/cart/{{ Auth::user()->id }}/update" method="Post">
+                                    <!-- {{csrf_field()}} -->
+                                    @forelse ($cartDetail as $cart)
+                                        <tr>
+                                            <td class="cart_product_img">
+                                                <a href="{{ url('/product-details', $cart->product_id) }}"><img src="{{ asset('images/'.$cart->product->photos->first()->image_name) }}" alt="Product"></a>
+                                            </td>
+                                            <td class="cart_product_desc">
+                                                <h5>{{ $cart->product->product_name }}</h5>
+                                            </td>
+                                            <td class="price">
+                                                <span>IDR {{ $cart->product->price }}</span>
+                                            </td>
+                                            <td class="qty">
+                                                <div class="qty-btn d-flex">
+                                                    <p>Qty</p>
+                                                    <div class="quantity">
+                                                        {{csrf_field()}}
+                                                        <span class="qty-minus" onclick="var effect = document.getElementById('qty{{ $loop->iteration }}'); var qty = effect.value; if( !isNaN( qty ) &amp;&amp; qty &gt; 1 ) effect.value--;return false;"><i class="fa fa-minus" aria-hidden="true"></i></span>
+                                                        <input type="number" class="qty-text" id="qty{{ $loop->iteration }}" step="1" min="1" max="{{ $cart->product->qty}}" name="quantity[]" value="{{ $cart->qty }}">
+                                                        <span class="qty-plus" onclick="var effect = document.getElementById('qty{{ $loop->iteration }}'); var qty = effect.value; if( !isNaN( qty )) effect.value++;return false;"><i class="fa fa-plus" aria-hidden="true"></i></span>
+                                                    </div>
+                                                    <a href="/cart/{{ $cart->id }}/delete"><i class="fa fa-trash" style="font-size:35px;color:grey;"></i></a>
                                                 </div>
-                                                <a href="/cart/{{$cart->id }}/delete"><i class="fa fa-trash" style="font-size:35px;color:grey;"></i></a>
-                                            </div>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <td>
+                                            <p style="text-align: center">No data available in Cart</p>
                                         </td>
-                                    </tr>
-                                @empty
-                                    <td>
-                                        <p style="text-align: center">No data available in Cart</p>
-                                    </td>
-                                @endforelse
+                                    @endforelse
+                                        <button type="submit" class="btn amado-btn mb-15" href="/cart">Update Cart</button>
+                                    </form>
                             </tbody>
                         </table>
                     </div>
@@ -75,9 +79,9 @@
                         <div class="cart-summary">
                             <h5>Cart Total</h5>
                             <ul class="summary-table">
-                                <li><span>subtotal:</span> <span>IDR</span></li>
+                                <li><span>subtotal:</span> <span>IDR {{ $subTotal }}</span></li>
                                 <li><span>delivery:</span> <span>Free</span></li>
-                                <li><span>total:</span> <span>IDR</span></li>
+                                <li><span>total:</span> <span>IDR {{ $subTotal }}</span></li>
                             </ul>
                             <div class="cart-btn mt-100">
                                 <a href="/checkout/{{ Auth::user()->id }}" class="btn amado-btn w-100">Checkout</a>
